bd96b6000df3465733532c98820a7cd60a8fdd5e
Danilo Arroteia <sikor12@gmail.com>
2021-04-22 18:14:44 -0300

    Yes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ const routes = require('./routes');
 const app = express();
 
 //falando para express ir no corpo da req, cconverter o objeto em json
-app.use(cors);
+app.use(cors(/*{
+    oring :'http://meuapp.com';
+}*/));
 app.use(express.json());
 app.use(routes);
 
